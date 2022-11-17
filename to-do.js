76bab479d649bad67633564b90ea48ddf0b6b85d
Roschelle Matthews-Rhoden <roschelle2k6@gmail.com>
2022-11-17 02:19:11 -0500

    Impleemnted ability to add tasks

--- to-do.js
+++ to-do.js
@@ -1,4 +1,25 @@
 window.addEventListener("load", function(){
+    let task;
+    let add_btn = document.querySelector(".add");
+    // let del_btn = document.querySelector(".delete");
+    let message = document.querySelector(".result");
+    let user_input = document.getElementById("task_input");
     
+
+    add_btn.addEventListener("click", function(e){
+        e.preventDefault();
+        user_task = user_input.value;
+
+        addTask(user_task);
+    })
+
+    // del_btn.addEventListener
+
+    function addTask(user_task){
+        console.log(user_task);
+        task = `<div class="task"> <p>${user_task}</p> <button class="delete">Delete</button></div>`;
+        console.log(task);
+        message.innerHTML = task;
+    }
 
 })
