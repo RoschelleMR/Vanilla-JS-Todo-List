908a098cc982daaaaeec79aea9e45d240616d452
Roschelle Matthews-Rhoden <roschelle2k6@gmail.com>
2022-11-25 01:14:31 -0500

    Fixed addTask functionality and delete task functionality

--- to-do.js
+++ to-do.js
@@ -1,25 +1,54 @@
-window.addEventListener("load", function(){
-    let task;
+document.addEventListener("DOMContentLoaded", function(){
     let add_btn = document.querySelector(".add");
-    // let del_btn = document.querySelector(".delete");
-    let message = document.querySelector(".result");
+    
+    let message = document.querySelector("#result");
     let user_input = document.getElementById("task_input");
+
+    
+    function createTaskElement(userTask){
+
+        /* creates the tasks directly into the html dom
+            so it can be manipulated by js
+        */
+
+        html = userTask
+
+        const div = document.createElement("div")
+        div.className = "task"
+        document.getElementById("result").appendChild(div)
+
+        const p = document.createElement("p")
+        p.textContent = html
+        div.appendChild(p)
+
+        const button = document.createElement("button")
+        button.className = "delete"
+        button.setAttribute("type","submit")
+        button.textContent = "Delete"
+        div.appendChild(button)
+
+    }
     
 
     add_btn.addEventListener("click", function(e){
         e.preventDefault();
         user_task = user_input.value;
 
-        addTask(user_task);
+        createTaskElement(user_task);
+    
     })
 
-    // del_btn.addEventListener
+    
+    // delete functionality
+    message.addEventListener("click", function(e){
+        if (e.target.classList.contains('delete')){
+            e.preventDefault();
+            button = e.target
+            console.log(button.parentNode)
+            button.parentNode.remove()
+        }
+        
+    })
 
-    function addTask(user_task){
-        console.log(user_task);
-        task = `<div class="task"> <p>${user_task}</p> <button class="delete">Delete</button></div>`;
-        console.log(task);
-        message.innerHTML = task;
-    }
 
 })
